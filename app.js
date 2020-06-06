10f67ca9819e6f41dd1720cc6a55ab5df3f71781
PauloMenegasso <64872656+PauloMenegasso@users.noreply.github.com>
2020-06-06 17:24:28 -0300

    Add files via upload
    music and dinamic level added
    style changed

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
     const scoreDisplay = document.querySelector('#score')
+    const highScoreDisplay = document.querySelector('#hi-score')
+    const levelDisplay = document.querySelector('#level')
     const startBtn = document.querySelector('#start-btn')
+    const restartBtn = document. querySelector('#restart-btn')
     const width = 10
     let nextRandom = 0
     let TimerId = 0
-    let levelInterval = 1000
+    var levelInterval = 500
     let score = 0
+    let highScore = 0
+    let level = 0
+    let nextlevel = 1
+    const colors = [
+        'orange',
+        'red',
+        'purple',
+        'green',
+        'blue'
+    ]
+    const myMusic = new sound("src/music.mp3");
+    const rotateSound = new sound("src/rotate.mp3");
+    const rowSound = new sound("src/row_eliminated.mp3");
+    const takenSound = new sound("src/taken.mp3");
+    var i = 1;
 
     // Defining Tetrominoes
     const lTetromino = [
@@ -75,12 +93,14 @@ document.addEventListener('DOMContentLoaded', () => {
     function draw() {
         current.forEach(index =>{
             squares[currentPosition + index].classList.add('tetromino')
+            squares[currentPosition + index].style.backgroundColor = colors[random]
         })
     }
 // undraw
     function undraw() {
         current.forEach(index =>{
             squares[currentPosition + index].classList.remove('tetromino')
+            squares[currentPosition + index].style.backgroundColor = ''
         })
     }
 
@@ -92,6 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPosition = currentPosition + width
         draw()
         freeze()
+        console.log(levelInterval)
     } 
 
 //freeze function
@@ -107,6 +128,8 @@ document.addEventListener('DOMContentLoaded', () => {
             displayShape()
             addScore()
             gameOver()
+            takenSound.play()
+            createLevel()
         }
     }
     //move the tetromino left, unless on the edge
@@ -145,12 +168,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         current = theTetrominos[random][currentRotation]
         draw()
+        rotateSound.play()
     }
 
 //next tetromino on minigrid
     const displaySquares = document.querySelectorAll('.mini-grid div')
     const displayWidth = 4
-    let displayIndex = 0
+    const displayIndex = 0
 
 //the Tetromino without rotations
     const upNextTetrominoes = [
@@ -165,10 +189,12 @@ document.addEventListener('DOMContentLoaded', () => {
     function displayShape() {
         displaySquares.forEach(square => {
             square.classList.remove('tetromino')
+            square.style.backgroundColor = ''
         })
 
         upNextTetrominoes[nextRandom].forEach( index => {
             displaySquares[displayIndex + index].classList.add('tetromino')
+            displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
         })
     }
 
@@ -177,11 +203,14 @@ document.addEventListener('DOMContentLoaded', () => {
         if (timerId){
             clearInterval(timerId)
             timerId = null
+            myMusic.stop()
         } else{
             draw()
             timerId= setInterval(moveDown, levelInterval)
             nextRandom = Math.floor(Math.random()*theTetrominos.length)
             displayShape()
+            myMusic.play()
+
         }
     })   
     
@@ -193,10 +222,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if(row.every(index => squares[index].classList.contains('taken'))) {
                 score +=10
+                rowSound.play()
                 scoreDisplay.innerHTML = score
                 row.forEach(index => {
                     squares[index].classList.remove('taken')
                     squares[index].classList.remove('tetromino')
+                    squares[index].style.backgroundColor = ''
                 })
                 const squaresRemoved = squares.splice(i, width)
                 squares = squaresRemoved.concat(squares)
@@ -210,8 +241,70 @@ document.addEventListener('DOMContentLoaded', () => {
         if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
             scoreDisplay.innerHTML = 'end'
             levelInterval = 111111515151
-            alert('Game Over')
+            if(score > highScore){
+                highScore = score
+                highScoreDisplay.innerHTML = highScore
+            }
+        i = 2
+
         }
     }
 
+
+//function sound
+    function sound(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+        this.play = function(){
+        this.sound.play();
+        }
+        this.stop = function(){
+        this.sound.pause();
+        }
+    }
+
+//making levels
+    function createLevel() {
+        if(score >= 10){
+            level = 1
+            timerId= setInterval(moveDown, 500)
+ }
+        if (score >= 30){
+            level = 2
+            nextlevel = 2
+            clearInterval(timerId)
+            timerId = null
+            timerId= setInterval(moveDown, 400)
+  }
+        if (score >= 50){
+            level = 3
+            nextlevel = 3
+            timerId= setInterval(moveDown, 300)
+}
+        if (score >= 120){
+            timerId= setInterval(moveDown, 200)
+        }
+    
+        levelDisplay.innerHTML = level
+    
+    }
+
+//increase level
+function increaseLevel() {
+        levelInterval -= 100
+    }
+
+//função restart
+    restartBtn.addEventListener('click', () => {
+        if(i == 2){
+        score = 0
+        let currentPosition = 4
+        let currentRotation = 0
+        let squares = Array.from(document.querySelectorAll('.grid div'))
+        }
+    })
 })
